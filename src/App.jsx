c60c8c42a854fe33e992629828326d1d577e1e7a
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2024-09-19 15:25:49 +0530

    new details added

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
     >
       <h1 className="text-3xl mb-6">Weather App</h1>
       <div>
-        <div className="flex gap-6 mb-6">
+        <div className="flex gap-6 mb-6 justify-center">
           <input
             type="text"
             onChange={handleInputChange}
@@ -52,10 +52,23 @@ const App = () => {
             Search
           </button>
         </div>
-        <div className="mb-7 px-8">
+
+        <div className='flex gap-11 mb-6' >
+    <div className="bg-white w-[25%] m-7 p-8 shadow-[5px_5px_25px_-5px_rgba(0,0,0,0.5)] rounded-[15px]">
           <h2 className="text-xl">{data?.location?.name}</h2>
           <img src={data?.current?.condition?.icon} alt="weather icon" />
           <h4 className="text-lg">{data?.current?.temp_c}°C</h4>
+          <h4 className="text-lg" > {data?.location?.region} </h4>
+          <h4 className="text-lg" > {data?.location?.country} </h4>
+        </div>
+    <div className="bg-white w-[54%%] m-7 p-8 shadow-[5px_5px_25px_-5px_rgba(0,0,0,0.5)] rounded-[15px]">
+          <h4 className="text-xl"> Localtime- {data?.location?.localtime}</h4>
+          <h4 className="text-lg"> Humidity- {data?.current?.humidity} </h4>
+          <h4 className="text-lg"> Windchill- {data?.current?.windchill_c} </h4>
+          <h4 className="text-lg"> Wind-direction- {data?.current?.wind_dir}</h4>
+          <h4 className="text-lg"> Feels-Like- {data?.current?.feelslike_c}</h4>
+          <h4 className="text-lg"> Last-Updated- {data?.current?.last_updated}</h4>
+        </div>  
         </div>
       </div>
     </div>
